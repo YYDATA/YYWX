c612a38c674bbbb64f7683b6e5a740f22a7c0856
MRCai001002003 <294042223@qq.com>
2016-06-06 11:45:29 +0800

    CZ

--- src/main/java/com/yy/service/LoanOrderService.java
+++ src/main/java/com/yy/service/LoanOrderService.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.yy.common.exception.CustomException;
 import com.yy.dao.LoanOrderDao;
 import com.yy.domain.entity.Customer;
 import com.yy.domain.entity.LoanOrder;
@@ -20,7 +22,38 @@ public class LoanOrderService {
 	@Autowired
 	private CustomerService customerService;
 	public void saveOrUpLoadOrder(HttpServletRequest request,LoanOrder loanOrder){
-
+//		if(customer!=null)
+//			request.getSession().setAttribute("customer", customer);
+		if(StringUtils.isBlank(loanOrder.getLoanOrderCode())){
+			loanOrder.setOrderDate(new Date());
+			loanOrder.setLoanOrderCode(getLoanOrderCode());
+			loanOrderDao.insert(loanOrder);
+		}else{
+			loanOrderDao.updateByPrimaryKeySelective(loanOrder);
+		}
+	}
+	/**
+	 * @Title: saveCustomerLoan 
+	 * @Description: 保存信息 
+	 * @author caiZhen
+	 * @date 2016年6月6日 上午11:09:46
+	 * @param @param request
+	 * @param @param loanOrder    设定文件 
+	 * @return void    返回类型 
+	 */
+	public void saveCustomerLoan(HttpServletRequest request,LoanOrder loanOrder){
+		String verificationCode = (String)StringUtil.getSession(request, "verificationCode");
+		String phone = (String)StringUtil.getSession(request, "phone");
+		if(StringUtils.isBlank(verificationCode)||StringUtils.isBlank(phone)){
+			throw new CustomException("验证码已过期,请重置");
+		}
+		if(!phone.equals(loanOrder.getCellPhone())){
+			throw new CustomException("您输入手机号与收验证码手机号不一致");
+		}
+		String code=request.getParameter("code");
+		if(!verificationCode.equals(code)){
+			throw new CustomException("请核对验证码");
+		}
 		//增加贷款记录前，查看有无客户信息(有：获取客户id，无：增加)
 		Customer customer = new Customer(loanOrder.getCellPhone());
 		List<Customer> listCustomer = customerService.getCustomer(customer);
@@ -32,13 +65,10 @@ public class LoanOrderService {
 			customerService.saveOrUpCustomer(request,customer);
 			loanOrder.setCustomerID(customer.getCustomerID());
 		}
-//		if(customer!=null)
-//			request.getSession().setAttribute("customer", customer);
-		loanOrder.setOrderDate(new Date());
-		loanOrder.setLoanOrderCode(getLoanOrderCode());
-		loanOrderDao.insert(loanOrder);
+		saveOrUpLoadOrder(request,loanOrder);
 	}
 	/**
+	 * 
 	 * @return BA+年月日+6位流水号
 	 */
 	private String getLoanOrderCode(){
